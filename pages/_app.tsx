5cffee1fce2e63ee506ff3095edd48dd014424c6
saiani1 <saiani@naver.com>
2025-05-22 17:05:20 +0900

    한자 폰트 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import type { AppProps } from "next/app";
+import { Zen_Antique_Soft } from "next/font/google";
 
 import "@/styles/globals.css";
 
+const zen = Zen_Antique_Soft({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={zen.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default App;
